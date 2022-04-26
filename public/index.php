4e35c309b342a467245781d970f606e39fba9015
Joseph Clark <weirdlookingjay@live.com>
2022-04-26 12:04:22 -0400

    Created Front controller and Router Class

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Index";
+/**
+ * Front Controller
+ */
+//echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+
+/**
+ * Routing
+ */
+require '../Core/Router.php';
+$router = new Router();
+echo get_class($router);
